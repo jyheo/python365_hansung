d7a9251949857f5a689e308a273b27738a052564
jyheo <jyheo0@gmail.com>
2013-07-17 20:58:00 +0900

    add NewId for ID generation

--- wxPython/wxPython01_button.py
+++ wxPython/wxPython01_button.py
@@ -12,14 +12,14 @@ class MainFrame(wx.Frame):
 
     def initWidget(self):
         # 1. Create Button
-        btn = wx.Button(self, label='Hello')
+        btn = wx.Button(self, id=wx.NewId(), label='Hello')
 
         # 2. Bind button click event
         btn.Bind(wx.EVT_BUTTON, self.onHello)
 
     def onHello(self, event):
         btn = event.GetEventObject()
-        print 'Button pressed:', btn.GetLabelText()
+        print 'Button pressed:', btn.GetLabelText(), btn.GetId()
        
 
 def main():
