3e94ec062c1e6a9146aa4177260921e9059b04c1
jyheo <jyheo0@gmail.com>
2013-07-19 01:48:01 +0900

    print more info when it receives data

--- socket/echoclient.py
+++ socket/echoclient.py
@@ -1,11 +1,12 @@
 import socket
 
 host = 'localhost'
-port = 50000
+port = 8123
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-s.send('Hello, world')
+sent = s.send('Hello, world')
+print 'Sent %d bytes' % (sent)
 data = s.recv(size)
 s.close()
-print 'Received:', data
+print 'Received:', len(data), 'bytes, Contents:', data
--- socket/echoserver.py
+++ socket/echoserver.py
@@ -1,7 +1,7 @@
 import socket
 
 host = ''
-port = 50000
+port = 8123
 backlog = 5
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,8 +9,12 @@ s.bind((host,port))
 s.listen(backlog)
 while 1:
     client, address = s.accept()
+	# client is an instance of socket
     data = client.recv(size)
     if data:
-        print data
+        print 'Received:', len(data), 'bytes, Contents:', data
         client.send(data)
     client.close()
+
+# Never here! ;-)
+s.close()
