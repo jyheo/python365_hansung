4af90953b551d0ea0a2b490b7a7c2cb9e8ed120e
jyheo <jyheo0@gmail.com>
2013-07-18 12:44:11 +0900

    add menu event

--- wxPython/wxPython09_events.py
+++ wxPython/wxPython09_events.py
@@ -7,9 +7,24 @@ class MainFrame(wx.Frame):
 
         self.title = 'Events'
         self.SetTitle(self.title)
+        self.initMenu()
         self.initWidget()
         self.Center()
         self.Show()
+
+    def initMenu(self):
+        menubar = wx.MenuBar()
+        fileMenu = wx.Menu()
+
+        # file Menu
+        fileMenu.Append(wx.ID_NEW, '&New', 'New file')
+        fileMenu.Append(wx.ID_OPEN, '&Open', 'Open a file')
+        fileMenu.Append(wx.ID_EXIT)
+
+        self.Bind(wx.EVT_MENU, self.onCommand)
+
+        menubar.Append(fileMenu, '&File')
+        self.SetMenuBar(menubar)
 
     def initWidget(self):
         panel = wx.Panel(self)
@@ -27,15 +42,26 @@ class MainFrame(wx.Frame):
         panel.Bind(wx.EVT_SIZE, self.onSize)
         self.Bind(wx.EVT_CLOSE, self.onClose)
 
+    def onCommand(self, e):
+        # e is an instance of wx.CommandEvent
+        mid = e.GetId()
+        if mid == wx.ID_EXIT:
+            self.Close()
+        elif mid == wx.ID_NEW:
+            print 'New'
+        elif mid == wx.ID_OPEN:
+            print 'Open'
+
     def onMove(self, e):
+        # wx.MoveEvent
         x, y = e.GetPosition()
         self.st1.SetLabel(str(x))
         self.st2.SetLabel(str(y))
 
     def onSize(self, e):
+        # wx.SizeEvent
         w, h = e.GetSize()
         print "size", w, h
-        e.Skip()
 
     def onClose(self, e):
         dial = wx.MessageDialog(None, 'Are you sure to quit?', 'Question(onClose)',
@@ -49,6 +75,7 @@ class MainFrame(wx.Frame):
             e.Veto()
 
     def onKeyDown(self, e):
+        # wx.KeyEvent
         key = e.GetKeyCode()
         if key == wx.WXK_ESCAPE:
             ret = wx.MessageBox('Are you sure to quit?', 'Question(onKeyDown)', 
@@ -62,8 +89,9 @@ class MainFrame(wx.Frame):
         e.Skip()
 
     def onLeftDown(self, e):
+        # wx.MouseEvent
         print e.GetPosition()
-        e.Skip()
+        e.Skip()  # should e.Skip() because it is a focus related event!
 
 def main():
     app = wx.App(redirect=False)
